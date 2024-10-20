d9e5e374cfcce5e194cdd307d0e395bff0861c90
shchdenis <shchdenis@internet.ru>
2024-10-20 03:40:36 +0300

    virlab

--- env.js
+++
@@ -1,6 +0,0 @@
-window.env = {
-  "API_TELEGRAM": "https://api.telegram.org/bot7385458769:AAEMyKklaPFKJoEWYGtOu2U_iGT3mSgX34w/",
-  "CHAT_ID": "1940412095",
-  "TEST_TELEGRAM": "https://api.telegram.org/bot7825317543:AAGc_2UzsQ4rjrPFc1RwJHIxneGTkHFyldc/",
-  "TEST_ID": "1940412095"
-};
